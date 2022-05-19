0ec6ebfbed4f2d2602040b183fb1d27b0a0dca85
Jekyung Min <cwm7485@naver.com>
2022-05-19 13:45:11 +0900

    fix: react-router-dom v6에 맞게  테스트케이스 변경

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import React from 'react';
-import './App.css';
-// TODO - react-router-dom을 설치 후, import 구문을 이용하여 BrowserRouter, Route, Switch 컴포넌트를 불러오세요.
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Sidebar from './Sidebar';
-// TODO - import문을 이용하여 Mypage, About 컴포넌트를 불러오세요.
-import Mypage from './Pages/MyPage';
-import About from './Pages/About';
-import Tweets from './Pages/Tweets';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Tweets from "./Pages/Tweets";
+import MyPage from "./Pages/MyPage";
+import About from "./Pages/About";
 
 const App = () => {
   return (
     <div>
-      {/* TODO - BrowserRouter 컴포넌트를 작성합니다. */}
       <BrowserRouter>
         <div className="App">
           <main>
             <Sidebar />
             <section className="features">
-              {/* TODO - Switch와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
-              <Switch>
-                <Route exact path="/">
-                  <Tweets />
-                </Route>
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/mypage">
-                  <Mypage />
-                </Route>
-              </Switch>
+              <Routes>
+                <Route path="/" element={<Tweets />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/mypage" element={<MyPage />} />
+              </Routes>
             </section>
           </main>
         </div>
--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer>Copyright @ 2021 Code States</footer>;
+  return <footer>Copyright @ 2022 Code States</footer>;
 };
 
 export default Footer;
